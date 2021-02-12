296d77bece76e4773a1e1d7b9ec88ca731c5af6a
MichelleCaobianco <michellecaobianco@gmail.com>
2021-02-12 11:58:14 -0300

    new changes

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const dino = document.querySelector('.dino');
 
-document.addEventListener('keyup', function(){
-    console.log('pressionou tecla');
-});
+function handleKeyUp(event){
+    if (event.keyCode === 32){
+        console.log('Pressionou espaço!');
+    }
+}
+function jump(){
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150){
+
+        }
+        //subindo
+        position += 20;
+        dino.style.bottom = position + 'px';
+    },20);
+}
+
+document.addEventListener('keyup', handleKeyUp);
 
